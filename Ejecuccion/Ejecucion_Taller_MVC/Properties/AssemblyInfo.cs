040ac00c1e504249cc643ce61784d22c716b0e33
VictorSamayoa <josuesamayoa064@gmail.com>
2026-09-11 22:10:48 -0600

    Agregando Ejecutor

---
+++ Ejecuccion/Ejecucion_Taller_MVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ejecucion_Taller_MVC")]
+[assembly: AssemblyDescription("Proyecto de Ejecución para Taller de MVC - Victor Samayoa 0901-23-3424")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Risko / UMG")]
+[assembly: AssemblyProduct("Ejecucion_Taller_MVC")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("392f32b7-791d-4303-9b01-fd2faf0ff037")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
